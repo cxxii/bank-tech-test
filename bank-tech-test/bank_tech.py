2fbccce74f2fe8bbdb3d07e398b3b8e565b0c666
Callum <Callum@hotmail.com>
2023-04-19 17:31:42 +0100

    initial commit

---
+++ bank-tech-test/bank_tech.py
@@ -0,0 +1,134 @@
+class Bank:
+    def __init__(self):
+        self.balance = []
+        self.date = []
+        self.amount = []
+
+    def deposit(self, amount, date):
+        """
+        Deposits given amount into bank
+
+        Args:
+            amount (int, float): amount to deposit in the bank.
+            date (string): Date on which transaction occurs.
+
+        Raises:
+            TypeError: If amount is not numerical
+            Exception: If amount is <= 0
+        """
+
+        # Error handling
+        match amount:
+            case n if not isinstance(n, (float, int)):
+                raise TypeError("ONLY ENTER NUMERICAL VALUES")
+            case n if n == 0:
+                raise Exception("CAN NOT DEPOSIT 0")
+            case n if n < 0:
+                raise Exception("CAN NOT DEPOSIT NEGATIVE MONEY")
+
+        if len(self.balance) > 0:
+            self.balance.append(self.balance[-1] + amount)
+        else:
+            self.balance.append(amount)
+
+        # Deposit function
+        self.date.append(date)
+        self.amount.append(amount)
+
+    def withdraw(self, amount, date):
+        """
+        Function withdraws given amount of money from the bank account.
+
+        Args:
+            amount (float, int): amount to withdraw from bank
+            date (string): Date on which transaction occurs
+
+        Raises:
+            TypeError: If amount is not numerical
+            Exception: If amount is <= 0 and if amount is  > bank balance
+        """
+
+        # Error Handling
+        match amount:
+            case n if not isinstance(n, (float, int)):
+                raise TypeError("ONLY ENTER NUMERICAL VALUES")
+            case n if n == 0:
+                raise Exception("CAN NOT WITHDRAW 0")
+            case n if n < 0:
+                raise Exception("CAN NOT WITHDRAW NEGATIVE MONEY")
+            case n if n > self.balance[-1]:
+                raise Exception("INSUFFICIENT FUNDS")
+
+        # Widthdrawl function
+        if len(self.balance) == 0:
+            raise Exception("INSUFFICIENT FUNDS")
+        else:
+            self.date.append(date)
+            self.amount.append(-abs(amount))
+
+        if self.balance:
+            self.balance.append(float(self.balance[-1] - amount))
+        else:
+            self.balance.append(float(amount))
+
+    def statement(self):
+        """
+        Prints the account statement in a formatted table
+
+        Returns:
+            _type_: _description_
+        """
+
+        horizontal_line = "." * 69
+
+        print(horizontal_line)
+        print(
+            "||",
+            "DATE".center(12),
+            "||",
+            "CREDIT".center(13),
+            "||",
+            "DEBIT".center(13),
+            "||",
+            "BALANCE".center(13),
+            "||",
+        )
+        print(horizontal_line)
+
+        for date, amount, balance in zip(
+            reversed(self.date), reversed(self.amount), reversed(self.balance)
+        ):
+            if amount > 0:
+                credit = "{:.2f}".format(amount).rjust(13)
+                debit = " ".rjust(13)
+            else:
+                credit = " ".rjust(13)
+                debit = "{:.2f}".format(abs(amount)).rjust(13)
+
+            print(
+                "||",
+                date.rjust(12),
+                "||",
+                credit,
+                "||",
+                debit,
+                "||",
+                "{:.2f}".format(balance).rjust(13),
+                "||",
+            )
+
+        print(horizontal_line)
+
+        return ""
+
+
+def main():
+    bank = Bank()
+    bank.deposit(22425.27, "31/10/91")
+    bank.deposit(365, "21/11/22")
+    bank.withdraw(3389.55, "13/2/22")
+    print(bank.statement())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/bank_tech_tests.py
@@ -0,0 +1,73 @@
+import unittest
+import pytest
+from  import Bank
+
+
+
+class TestBank(unittest.TestCase):
+    def setUp(self):
+        self.bank = Bank()
+
+    # Tests depositing function
+    def test_deposit(self):
+        self.bank.deposit(100, "01/01/2022")
+        assert self.bank.balance == [100]
+        assert self.bank.date == ["01/01/2022"]
+        assert self.bank.amount == [100]
+
+    # Test withdraw function
+    def test_withdraw(self):
+        self.bank.deposit(100, "01/01/2022")
+        self.bank.withdraw(50, "02/01/2022")
+        assert self.bank.balance == [100, 50]
+        assert self.bank.date == ["01/01/2022", "02/01/2022"]
+        assert self.bank.amount == [100, -50]
+
+    # Tests attemping to a deposit negative amount
+    def test_deposit_negative(self):
+        with pytest.raises(Exception, match="CAN NOT DEPOSIT NEGATIVE MONEY"):
+            self.bank.deposit(-1, "02/01/2022")
+
+    # Tests attemping to a withdraw negative amount
+    def test_withdraw_negative(self):
+        with pytest.raises(Exception, match="CAN NOT WITHDRAW NEGATIVE MONEY"):
+            self.bank.withdraw(-1, "02/01/2022")
+
+    # Tests trying to go overdrawn
+    def test_overdrawn(self):
+        self.bank.deposit(100, "01/01/2022")
+        with pytest.raises(Exception, match="INSUFFICIENT FUNDS"):
+            self.bank.withdraw(101, "02/01/2022")
+            assert self.bank.balance[-1] == 100
+
+    # Tests trying to go withdraw 0
+    def test_withdraw_0(self):
+        self.bank.deposit(100, "01/01/2022")
+        with pytest.raises(Exception, match="CAN NOT WITHDRAW 0"):
+            self.bank.withdraw(0, "02/01/2022")
+            assert self.bank.balance[-1] == 100
+
+    # Tests trying to deposit 0
+    def test_deposit_0(self):
+        self.bank.deposit(100, "01/01/2022")
+        with pytest.raises(Exception, match="CAN NOT DEPOSIT 0"):
+            self.bank.deposit(0, "02/01/2022")
+            assert self.bank.balance[-1] == 100
+
+    # Tests the ability to enter only numerical values
+    def test_alpha_deposit(self):
+        self.bank.deposit(100, "01/01/2022")
+        with pytest.raises(Exception, match="ONLY ENTER NUMERICAL VALUES"):
+            self.bank.deposit("G", "01/01/2022")
+            assert self.bank.balance == [100]
+
+    # Tests the ability to enter only numerical values
+    def test_alpha_withdraw(self):
+        self.bank.deposit(100, "01/01/2022")
+        with pytest.raises(Exception, match="ONLY ENTER NUMERICAL VALUES"):
+            self.bank.withdraw("G", "01/01/2022")
+            assert self.bank.balance == [100]
+
+
+if __name__ == "__main__":
+    unittest.main()
